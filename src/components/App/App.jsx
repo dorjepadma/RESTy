4b00fed12ec1bd91655ae905079dfd69da94e1fa
dorjepadma <dorjepadma@gmail.com>
2020-05-04 00:17:27 -0700

    displaying input

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Request from '../Request/Request.js';
+import Request from '../Request/Request';
 
 export default function App() {
   return <Request />;
--- src/components/Request/Request.js
+++ src/components/Request/Request.js
@@ -2,53 +2,72 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Request.css';
 
-// const RadioButton = ({ value, onChange, }) => (
-//   <>
-//     <input id ={value} type='radio' name={name} value={value} onChange={onChange} />
-//     <label htmlFor={value}>{value}</label>
-//   </>
-  
-// );
-
-const Request = ({ url, onUrlChange, body, onBodyChange, onMethodChange, onSubmit }) => (
-  <form onSubmit={onSubmit}>
+const Request = ({ url, body, onChange, onSubmit }) => (
+  <>
     <form className={styles.Request} onSubmit={onSubmit}>
-      <input type='text' value={url} onChange={onUrlChange} />
-    
-      <label>
-        POST:
-        <input type='radio' name='method' value='POST' onChange={onMethodChange} /> 
+      
+      <label> URL:
+        <input type="text" value={url} onChange={onChange} />
       </label>
-      <label>
-        GET:
-        <input type='radio' name='method' value='GET' onChange={onMethodChange} /> 
-      </label>
-      <label>
-        PATCH:
-        <input type='radio' name='method' value='PATCH' onChange={onMethodChange} /> 
-      </label>
-      <label>
-        PUT:
-        <input type='radio' name='method' value='PUT' onChange={onMethodChange} /> 
-      </label>
-      <label>
-        DELETE:
-        <input type='radio' name='method' value='POST' onChange={onMethodChange} /> 
-      </label>
-      <textarea value={body} onChange={onBodyChange}></textarea>
-      <button>Submit</button>
+      
+      <>
+        
+        <input 
+          type="radio" 
+          name="method" 
+          value="get" 
+          onChange={onChange}
+        /> 
+        <label> GET </label>
+        <input 
+          type="radio" 
+          name="method" 
+          value="post"
+          onChange={onChange}
+        /> 
+        <label> POST </label>
+        <input 
+          type="radio" 
+          name="method" 
+          value="put"
+          onChange={onChange} 
+        />
+        <label> PUT </label>
+        <input 
+          type="radio" 
+          name="method" 
+          value="patch" 
+          onChange={onChange}
+        /> 
+        <label> PATCH </label>
+        <input 
+          type="radio" 
+          name="method" 
+          value="delete"
+          onChange={onChange}
+        /> 
+        <label>DELETE </label>
+      </>
+
+      <textarea 
+        placeholder="JSON Body"
+        name="body"
+        value={body}
+        onChange={onChange}
+      >
+      </textarea>
+
+      <button type="submit">Submit</button>
+
     </form>
-  </form>
+  </>
 );
 
 Request.propTypes = {
   url: PropTypes.string.isRequired,
-  method: PropTypes.string.isRequired,
-  body: PropTypes.object.isRequired,
-  onUrlChange: PropTypes.function.isRequired,
-  onMethodChange: PropTypes.function.isRequired,
-  onBodyChange: PropTypes.function.isRequired,
-  onSubmit: PropTypes.func.isRequired
+  body: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
 };
 
 export default Request;
