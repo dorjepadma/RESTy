9486b93fe89cfc2ce0c31a9e49c90675ce48806b
dorjepadma <dorjepadma@gmail.com>
2020-05-08 15:47:56 -0700

    handle change built

--- src/container/Resty.jsx
+++ src/container/Resty.jsx
@@ -5,11 +5,21 @@ const Resty = () => {
   const {url, setUrl} = useState('')
   const {method, setMethod} = useState('Get')
   const {body, setBody} = useState('')
-}
+
+  const handleChange = ({ target }) => {
+    if(target.name === 'url') setUrl(target.value)
+    if(target.name === 'method') setMethod(target.value)
+    if(target.name === 'body') setBody(target.value)
+  }
 
   return (
     <>
-      <Request url={url} method={method} body={body} inChange={}onSubmit={this.handleSubmit}/>
+      <Request 
+        url={url} 
+        method={method} 
+        body={body} 
+        onChange={}
+        onSubmit={this.handleSubmit}/>
     </>  
   )
 };
