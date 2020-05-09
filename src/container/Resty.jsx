47b08530012a2b3e7da905e8a8de2aa213263a96
dorjepadma <dorjepadma@gmail.com>
2020-05-08 18:53:33 -0700

    response working

--- src/container/Resty.jsx
+++ src/container/Resty.jsx
@@ -1,24 +1,27 @@
 import React, { useState } from 'react';
 import Request from '../components/Request/Request';
 import { makeRequest } from '../services/Request';
+import Response from '../components/Response';
 
 const Resty = () => {
   const [url, setUrl] = useState('');
   const [method, setMethod] = useState('GET');
   const [body, setBody] = useState('');
+  const [response, setResponse] = useState({});
 
   const handleChange = ({ target }) => {
     if(target.name === 'url') setUrl(target.value);
     if(target.name === 'method') setMethod(target.value);
     if(target.name === 'body') setBody(target.value);
+
   };
 
   const handleSubmit = event => {
     event.preventDefault();
     makeRequest(url, method, body)
-      .then(json => console.log(json));
+      .then(json => setResponse(json));
   };
-  
+
   return (
     <>
       <Request
@@ -26,8 +29,10 @@ const Resty = () => {
         method={method} 
         body={body} 
         onChange={handleChange}
-        onSubmit={handleSubmit}/>
-    </>  
+        onSubmit={handleSubmit} />
+
+      <Response response={response} />
+    </> 
   );
 };
 
