0748f789102a34e52f2428293cef9c0afd898cb8
dorjepadma <dorjepadma@gmail.com>
2020-05-18 08:11:23 -0700

    refactor provider

--- src/hooks/RestProvider.jsx
+++ src/hooks/RestProvider.jsx
@@ -3,10 +3,18 @@ import React, { createContext } from 'react';
 const RestContext = createContext();
 
 export const RestProvider = ({ children }) => {
+  const [url, setUrl] = useState('');
+  const [method, setMethod] = useState('GET');
+  const [body, setBody] =useState('');
+
 
   return (
-      <RestContext.Provider value={}>
+    <RestContext.Provider value={ url, method, body}>
       {children}
-      </RestContext.Provider>
-  )
+    </RestContext.Provider>
+  );
+};
+export const useRest = () => {
+  const context = useContext(RestContext);
+  return context;
 };
